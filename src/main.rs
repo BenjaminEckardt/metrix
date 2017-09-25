5b65263cd7f295dc67ba0b813fff8c8d6b48344d
Benjamin Eckardt <Benjamin.Eckardt@gmx.net>
2017-09-25 14:46:53 +0200

    analyse all .rs files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-use std::io::Cursor;
+extern crate glob;
 
 mod loc;
 
+use std::fs::File;
+use std::io::BufReader;
+use glob::glob;
+
 fn main() {
-    let cursor = Cursor::new(b"lorem\nipsum\r\ndo\nlor");
-    loc::count_lines_of_code(cursor);
+    let lines = glob(&"./**/*.rs").unwrap().into_iter()
+        .map(|path_result| path_result.unwrap())
+        .filter(|path| !path.is_dir())
+        .map(|path| {
+            let file = File::open(path.clone()).unwrap();
+            let buf_reader = BufReader::new(file);
+            loc::count_lines_of_code(buf_reader)
+        });
+
+    for line in lines {
+        println!("{:?}", line);
+    }
 }
