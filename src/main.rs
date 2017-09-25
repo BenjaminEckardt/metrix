a5621669e5b673419429a81a3da22b9b5e7efcee
Benjamin Eckardt <Benjamin.Eckardt@gmx.net>
2017-09-25 15:19:11 +0200

    added prettytable for table output ... needs to be removed soon. messy deps.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
 extern crate glob;
+extern crate prettytable;
 
 mod loc;
 
 use std::fs::File;
 use std::io::BufReader;
 use glob::glob;
+use prettytable::Table;
+use prettytable::row::Row;
+use prettytable::cell::Cell;
 
 fn main() {
-    let lines = glob(&"./**/*.rs").unwrap().into_iter()
+    let mut t = Table::new();
+    t.add_row(Row::new(vec![Cell::new("path"), Cell::new("loc")]));
+    let table = glob(&"./**/*.rs").unwrap().into_iter()
         .map(|path_result| path_result.unwrap())
         .filter(|path| !path.is_dir())
         .map(|path| {
             let file = File::open(path.clone()).unwrap();
             let buf_reader = BufReader::new(file);
-            loc::count_lines_of_code(buf_reader)
+            (path, loc::count_lines_of_code(buf_reader))
+        }).fold(t, |mut result_table, path_and_count| {
+            let path = &path_and_count.0.display().to_string();
+            let count = &path_and_count.1.to_string();
+            result_table.add_row(Row::new(vec![Cell::new(path), Cell::new(count)]));
+            result_table
         });
 
-    for line in lines {
-        println!("{:?}", line);
-    }
+    table.printstd();
 }
